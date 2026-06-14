b25c27ae8cd55eb1a49f1b5c02fe4ec0a09dde04
ccxvo <ccxvoo@163.com>
2026-06-14 16:19:14 +0800

    exercise1

--- exercise.cpp
+++ exercise.cpp
@@ -1,32 +1,68 @@
 #include<iostream>
 using namespace std;
-#include<fstream>
 #include<string>
+#include<iomanip>
+#include<cstdlib>
+#include<fstream>
+#include<sstream>
 
+class Student{
+private:    
+string m_name;
+    int m_id;
+    int grades[5];
+    double m_average;
 
-void test01(){
+    public:
+    
+       void input(){
+        cout<<"请输入学生姓名："<<endl;
+        cin>>m_name;
+        this->m_name;
+        cout<<"请输入学生学号："<<endl;
+        cin>>m_id;
+        this->m_id;
+      
+        int sum=0;
+        for(int i=0;i<5;i++){
+            cout<<"请输入第"<<i+1<<"门课程成绩："<<endl;
+            cin>>this->grades[i];
+            sum+=this->grades[i];
+        }
+        this->m_average=(double)sum/5;}
 
-ifstream ifs;
-ifs.open("text.txt",ios::in);
-if(!ifs.is_open()){
-    cout<<"文件打开失败"<<endl;
-    return;
-}
+      string toString() const {
+        ostringstream oss;
+        oss << m_name << "；" << m_id;
+        for(int i = 0; i < 5; i++) {
+            oss << "；" << grades[i];
+        }
+        oss << "；" << fixed << setprecision(2) << m_average;
+        return oss.str();
+      }
 
-string buf;
-while ((getline(ifs,buf)))
-{
-    cout<<buf<<endl;
-}
+      void write(){
+        ofstream ofs("StudentGrade.txt", ios::app);
+        if(!ofs) {
+            cerr << "无法打开文件 StudentGrade.txt" << endl;
+            return;
+        }
+        ofs << toString() << endl;
 
+        ofs.close();
+      }
 
-ifs.close();
-}
-
-
+   
+};
 int main(){
+    system("chcp 65001 > nul");
 
-test01();
+    for(int i = 0; i < 4; i++){
+      cout << "输入第" << i+1 << "位学生信息" << endl;
+      Student s;
+      s.input();
+      s.write();
+    }
 
-    return 0;
+  return 0;
 }
